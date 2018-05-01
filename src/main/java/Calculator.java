6d5b5fc253e029d4663a9e110d58b9ee3ce45b80
stooggle <stuartodonnell@gmail.com>
2018-05-01 09:22:35 +0100

    Water Bottle Class Completed, Four Tests Passing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,13 +1,5 @@
 public class Calculator {
 
-//    private int num1;
-//    private int num2;
-
-//    public Calculator(int num1, int num2){
-//        this.num1 = num1;
-//        this.num2 = num2;
-//
-//    }
 
     public int multiply(int num1, int num2){
         return( num1 * num2);
